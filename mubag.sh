0da4ddbae6d88a476310428500d9fb16a16af0bb
ThothLogos <thothlogos@protonmail.com>
2020-01-27 20:04:27 -0500

    Refine and test parsing

--- mubag.sh
+++ mubag.sh
@@ -10,19 +10,22 @@ Usage: $(basename $0) [OPTION] [FILE]
  * Beware, all options decrypt and unpack the archive temporarily
 
 OPTIONS:\n
-  -a, --add FILE               Add FILE to archive, repack
-  -o, --out FILE               Print contents of FILE to STDOUT, repack
-  -e, --edit FILE              Open FILE in $EDITOR for modification, repack
+  -a, --add=FILE               Add FILE to archive, repack
+  -o, --out=FILE               Print contents of FILE to STDOUT, repack
+  -e, --edit=FILE              Open FILE in $EDITOR for modification, repack
 "
 }
 
-if [[ "$#" -le 1 ]] || [[ "$#" -ge 3 ]]; then
-    display_usage
-    exit 1
+if [[ "$#" -lt 1 ]] || [[ "$#" -ge 3 ]]; then
+  echo "error: incorrect number of args"
+  display_usage
+  exit 1
 fi
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
+    --help) display_usage; exit 0;;
+
     -a) add=true; FILE="$2"; shift 2;;
     -o) out=true; FILE="$2"; shift 2;;
     -e) edit=true; FILE="$2"; shift 2;;
@@ -30,9 +33,21 @@ while [ "$#" -gt 0 ]; do
     --add=*) add=true; FILE="${1#*=}"; shift 1;;
     --out=*) out=true; FILE="${1#*=}"; shift 1;;
     --edit=*) edit=true; FILE="${1#*=}"; shift 1;;
-    --add|--out|--edit) echo "$1 requires an argument" >&2; exit 1;;
+    --add|--out|--edit) echo "error: $1 requires an equal sign, ex: $1=FILE" >&2; exit 1;;
 
-    -*) echo "unknown option: $1" >&2; exit 1;;
+    -*) echo "unknown option: $1" >&2; display_usage; exit 1;;
     *) handle_argument "$1"; shift 1;;
   esac
 done
+
+if [[ $add ]];then
+  echo "Add true, FILE $FILE"
+fi
+
+if [[ $out ]];then
+  echo "Out true, FILE $FILE"
+fi
+
+if [[ $edit ]];then
+  echo "Edit true, FILE $FILE"
+fi
