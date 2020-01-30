c21eb9a290c599c084c9b9cd0254442137642608
thothlogos <thothlogos@protonmail.com>
2020-01-30 00:05:04 -0500

    Add verbose, handle duplicate additions, quiet gpg

--- mubag.sh
+++ mubag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# TODO: Add verbose mode for most progress messaging - need debug too? (prob not)
 # TODO: Complete --edit/-e functionality
-# TODO: Delete ZIPLOC from decrypt function, not needed probably (confirm)
+# TODO: Verify all early exits don't leave a zip behind.
+# TODO: Trap CTRL-C to attempt cleanups there as well
 
 source config.sh
 
@@ -34,7 +34,8 @@ fi
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
-    --help) display_usage; exit 0;;
+    -h|--help) display_usage; exit 0;;
+    -v|--verbose) verbose=true; shift 1;; 
 
     -b) existing=true; EXISTING="$2"; shift 2;;
     -a) add=true; FILE="$2"; shift 2;;
@@ -54,29 +55,29 @@ done
 
 main() {
   if [[ $add ]] && [[ -f $FILE ]];then
-    echo "${GRN}$(tput bold)ADD${RST} BEGIN: $FILE"
+    [[ $verbose ]] && echo "${GRN}$(tput bold)ADD${RST} BEGIN: $FILE"
     if [[ $EXISTING ]] && [[ -f $EXISTING ]]; then
-      echo "${GRN}$(tput bold)ADD${RST}: backup already exists at $EXISTING - decrypting"
+      [[ $verbose ]] && echo "${GRN}$(tput bold)ADD${RST}: backup already exists at $EXISTING - decrypting"
       decrypt_zip
       create_or_update_zip
       encrypt_zip
       secure_remove_file $ZIPLOC
     elif [[ -f $ZIPLOC.gpg ]]; then
-      echo "${GRN}$(tput bold)ADD${RST} ${RED}$(tput bold)FAIL${RST}: file $ZIPLOC.gpg exists but" \
-        "--existing-backup= is not set, please re-run with this option to confirm updating the existing file." \
-        "If you intended to create a new archive, please change the ZIPNAME in config.sh to something unique." \
-        "This check is to prevent accidental over-writing of a previously encrypted archive. Exiting."
+      echo "${RED}$(tput bold)ERROR${RST}: file $ZIPLOC.gpg exists but --existing-backup= is not set," \
+        "please re-run with this option to confirm updating the existing file. If you intended to create" \
+        "a new archive, please change the ZIPNAME in config.sh to something unique. This check is to" \
+        "prevent accidental over-writing of a previously encrypted archive. Exiting."
       exit 1
     else
-      echo "${GRN}$(tput bold)ADD${RST}: existing backup not found - creating new archive at $ZIPLOC"
+      [[ $verbose ]] && echo "${GRN}$(tput bold)ADD${RST}: existing backup not found - creating new archive at $ZIPLOC"
       create_or_update_zip
       encrypt_zip
       secure_remove_file $ZIPLOC
     fi
   elif [[ $out ]];then
-    echo "${MAG}$(tput bold)OUT${RST} BEGIN: $FILE"
+    [[ $verbose ]] && echo "${MAG}$(tput bold)OUT${RST} BEGIN: $FILE"
     if [[ $EXISTING ]] && [[ -f $EXISTING ]]; then
-      echo "${MAG}$(tput bold)OUT${RST}: backup already exists at $EXISTING - decrypting"
+      [[ $verbose ]] && echo "${MAG}$(tput bold)OUT${RST}: backup already exists at $EXISTING - decrypting"
       decrypt_zip
       output_requested_file_from_archive
       secure_remove_file $ZIPLOC
@@ -88,13 +89,13 @@ main() {
   elif [[ $edit ]];then
     echo "Edit true, FILE $FILE"
   else
-    echo "${GRN}$(tput bold)ADD${RST} ${RED}$(tput bold)FAIL${RST}: $FILE not found! Exiting."
+    echo "${RED}$(tput bold)ERROR${RST}: $FILE not found! Exiting."
     exit 1
   fi
 }
 
 output_requested_file_from_archive() {
-  echo "${MAG}$(tput bold)OUT${RST}: routing $FILE to STDOUT from ${EXISTING%????}"
+  [[ $verbose ]] && echo "${MAG}$(tput bold)OUT${RST}: routing $FILE to STDOUT from ${EXISTING%????}"
   echo "${WHT}$(tput bold)--- BEGIN OUTPUT ---${RST}"
   unzip -p ${EXISTING%????} $FILE
   echo "${WHT}$(tput bold)---  END OUTPUT  ---${RST}"
@@ -102,19 +103,20 @@ output_requested_file_from_archive() {
 
 secure_remove_file() {
   if command -v shred >/dev/null; then # prioritize secure removal over simple rm, if avail
-    echo "${YEL}$(tput bold)CLEANUP${RST}: shred exists on system, shredding $1"
+    [[ $verbose ]] && echo "${YEL}$(tput bold)CLEANUP${RST}: shred exists on system, shredding $1"
     shred -uz $1 # -u delete file, -v verbose, -z zero-out before deletion
     if [[ $? -eq 0 ]]; then
-      echo "${YEL}$(tput bold)CLEANUP${RST}: success, shred complete"
+      echo "${YEL}$(tput bold)CLEANUP${RST}: success, shred of $1 complete - decrypted archive is securely purged"
     else
       echo "${YEL}$(tput bold)CLEANUP${RST} ${RED}$(tput bold)FAIL${RST}:: shred failed!"
+      # TODO: fallback rm if shred really did fail
       exit 1
     fi
   else
-    echo "${YEL}$(tput bold)CLEANUP${RST} ${MAG}FALLBACK${RST}: secure file erasure shred not found, resorting to rm"
+    echo "${YEL}$(tput bold)CLEANUP${RST} ${RED}FALLBACK${RST}: secure file erasure shred not found, resorting to rm"
     rm $1
     if [[ $? -eq 0 ]]; then
-      echo "${YEL}$(tput bold)CLEANUP${RST}: success, rm of $1 complete"
+      echo "${YEL}$(tput bold)CLEANUP${RST}: success, rm of $1 complete - decrypted archive may be recoverable!"
     else
       echo "${YEL}$(tput bold)CLEANUP${RST} ${RED}$(tput bold)FAIL${RST}: rm failed!"
       exit 1
@@ -123,20 +125,29 @@ secure_remove_file() {
 }
 
 create_or_update_zip() {
+  if [[ $EXISTING ]]; then # check for potential duplicate file
+    local filename=$(basename $FILE)
+    unzip -l $ZIPLOC | grep -q $filename
+    if [[ $? -eq 0 ]]; then
+      echo "${RED}$(tput bold)ERROR${RST}: Unable to add $FILE to $ZIPLOC, filename already exists in the archive."
+      secure_remove_file $ZIPLOC
+      exit 1
+    fi
+  fi
   zip -u -r -j $ZIPLOC $FILE # -u update zip (if it exists), -j ignores dir structure of $FILE
   if [[ $? -eq 0 ]]; then
-    echo "${GRN}$(tput bold)ADD${RST}: create/update successful"
+    echo "${GRN}$(tput bold)ADD${RST}: archive creation/update successful"
   elif [[ $? -eq 12 ]]; then
-    echo "${GRN}$(tput bold)ADD${RST} ${YEL}$(tput bold)NO-OP${RST}:: zip update failed, likely file is already archived?"
+    echo "${GRN}$(tput bold)ADD${RST} ${YEL}$(tput bold)NO-OP${RST}:: zip update failed 'nothing to do'?"
   else
     echo "${GRN}$(tput bold)ADD${RST} ${RED}$(tput bold)FAIL${RST}:: unknown zip creation or update error! Exiting."
+    if [[ -f $ZIPLOC ]]; then secure_remove_file $ZIPLOC; fi
     exit 1
   fi
 }
 
 decrypt_zip() {
-  if [[ $EXISTING ]]; then local target=${EXISTING%????}; else local target=$ZIPLOC; fi
-  gpg --no-symkey-cache -o $target --decrypt $target.gpg
+  gpg -q --no-symkey-cache -o ${EXISTING%????} --decrypt $EXISTING
   if [[ $? -eq 0 ]]; then
     echo "$(tput bold)${CYN}DECRYPT${RST}: successful"
   else
@@ -146,7 +157,7 @@ decrypt_zip() {
 }
 
 encrypt_zip() {
-  gpg --no-symkey-cache --cipher-algo $ALGO --symmetric $ZIPLOC
+  gpg -q --no-symkey-cache --cipher-algo $ALGO --symmetric $ZIPLOC
   if [[ $? -eq 0 ]]; then
     echo "$(tput bold)${BLU}ENCRYPT${RST}: successful"
   else
