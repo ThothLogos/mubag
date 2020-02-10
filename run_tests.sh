440399eea3e0a655fba4404ace3c8c9a1a0ffb94
thothlogos <thothlogos@protonmail.com>
2020-02-10 02:36:58 -0500

    Add tests for add, list, print, extract, edit

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 TESTDIR="test_temp"
-
-
-
+TESTPASS="gogo"
 source config.sh
-
-display_usage() { echo "Usage: $(basename $0)"; }
 
 main() {
   setup
-
-
-
+  do_test "test_create_new_archive"
+  do_test "test_add_files_to_archive"
+  do_test "test_list_files_in_archive"
+  do_test "test_print_file_from_archive"
+  do_test "test_extract_file_from_archive"
+  do_test "test_edit_file_within_archive"
   #shutdown
 }
 
@@ -20,22 +19,20 @@ setup() {
   setup_echo "Setting up test environment... "
   if ! [ -d $TESTDIR ];then mkdir $TESTDIR;fi
   pushd $TESTDIR > /dev/null
-  for c in {a..c};do
-    local size=$(( 4096 + RANDOM % 60000 ))
+  for c in {A..C};do
+    local size=$(( 4096 + RANDOM % 4096 ))
     generate_text_file "$c.txt" $size
     if ! [[ $? -eq 0 ]];then err_echo "Unable to generate test files, aborting.";fi
   done
   ok_echo
   popd > /dev/null
   setup_echo "Running tests... \\n"
-  
-  do_test "test_create_new_archive"
 }
 
 shutdown() {
-  
   rm ./$TESTDIR/*
   rmdir $TESTDIR
+  rm activity.log
 }
 
 generate_text_file() {
@@ -53,11 +50,36 @@ do_test() {
   else
     fail_echo
   fi
+  sleep 0.1
 }
 
 test_create_new_archive() {
-  ./mubag.sh --test thisisonlyatest -o $TESTDIR/test.zip -a $TESTDIR/a.txt
+  ./mubag.sh -v --test $TESTPASS -o $TESTDIR/test.zip -a $TESTDIR/A.txt #>/dev/null
 }
+
+test_add_files_to_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -a $TESTDIR/B.txt #>/dev/null
+  ./mubag.sh -v --test $TESTPASS --backup=$TESTDIR/test.zip.gpg --add=$TESTDIR/C.txt #>/dev/null
+}
+
+test_list_files_in_archive() {
+  ./mubag.sh -v --test $TESTPASS --backup $TESTDIR/test.zip.gpg --list #>/dev/null
+}
+
+test_print_file_from_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -p A.txt
+}
+
+test_extract_file_from_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -x B.txt
+  rm B.txt
+}
+
+test_edit_file_within_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -e C.txt
+}
+
+
 
 test_echo()     { echo -ne "[${YL}TEST${RS}] Running \"$1\" ... "; }
 setup_echo()    { echo -ne "$*"; }
