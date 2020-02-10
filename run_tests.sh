deae2687ba0eeded4a53a6d46aa30ec98881ce1d
thothlogos <thothlogos@protonmail.com>
2020-02-10 04:01:50 -0500

    Add update, remove, failure to find file tests

--- run_tests.sh
+++ run_tests.sh
@@ -12,7 +12,11 @@ main() {
   do_test "test_print_file_from_archive"
   do_test "test_extract_file_from_archive"
   do_test "test_edit_file_within_archive"
-  #shutdown
+  do_test "test_failure_updating_unchanged_file"
+  do_test "test_update_file_within_archive"
+  do_test "test_remove_file_from_archive"
+  do_test "test_failure_to_find_file_in_archive"
+  shutdown
 }
 
 setup() {
@@ -32,7 +36,6 @@ setup() {
 shutdown() {
   rm ./$TESTDIR/*
   rmdir $TESTDIR
-  rm activity.log
 }
 
 generate_text_file() {
@@ -54,32 +57,54 @@ do_test() {
 }
 
 test_create_new_archive() {
-  ./mubag.sh -v --test $TESTPASS -o $TESTDIR/test.zip -a $TESTDIR/A.txt #>/dev/null
+  ./mubag.sh -v --test $TESTPASS -o $TESTDIR/test.zip -a $TESTDIR/A.txt >/dev/null
 }
 
 test_add_files_to_archive() {
-  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -a $TESTDIR/B.txt #>/dev/null
-  ./mubag.sh -v --test $TESTPASS --backup=$TESTDIR/test.zip.gpg --add=$TESTDIR/C.txt #>/dev/null
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -a $TESTDIR/B.txt >/dev/null
+  ./mubag.sh -v --test $TESTPASS --backup=$TESTDIR/test.zip.gpg --add=$TESTDIR/C.txt >/dev/null
 }
 
 test_list_files_in_archive() {
-  ./mubag.sh -v --test $TESTPASS --backup $TESTDIR/test.zip.gpg --list #>/dev/null
+  ./mubag.sh -v --test $TESTPASS --backup $TESTDIR/test.zip.gpg --list >/dev/null
 }
 
 test_print_file_from_archive() {
-  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -p A.txt
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -p A.txt >/dev/null
 }
 
 test_extract_file_from_archive() {
-  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -x B.txt
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -x B.txt >/dev/null
   rm B.txt
 }
 
 test_edit_file_within_archive() {
-  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -e C.txt
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -e C.txt >/dev/null
 }
 
+test_failure_updating_unchanged_file() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -x A.txt >/dev/null
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -u A.txt >/dev/null
+  rm A.txt
+  if ! [[ $? -eq 0 ]];then echo 0 >/dev/null;fi # Invert non-zero exit, we want failure to pass
+}
 
+test_update_file_within_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -x A.txt >/dev/null
+  echo "Ch-ch-ch-ch-changes" >> A.txt
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -u A.txt >/dev/null
+  rm A.txt
+}
+
+test_remove_file_from_archive() {
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -r B.txt >/dev/null
+}
+
+test_failure_to_find_file_in_archive() {
+  # attempt to print a non-existing file, expect failure
+  ./mubag.sh -v --test $TESTPASS -b $TESTDIR/test.zip.gpg -p DNE.void >/dev/null
+  if ! [[ $? -eq 0 ]];then echo 0 >/dev/null;fi # Invert non-zero exit, we want failure to pass
+}
 
 test_echo()     { echo -ne "[${YL}TEST${RS}] Running \"$1\" ... "; }
 setup_echo()    { echo -ne "$*"; }
